648dd753a5f3f094930066e8b34aa68cf417ee6a
milkway <leite@de.ufpe.br>
2019-03-29 14:36:30 -0300

    first commit

---
+++ src/SampleDecoder.cpp
@@ -0,0 +1,65 @@
+/*
+ * SampleDecoder.cpp
+ *
+ * For more information, see SampleDecoder.h
+ *
+ * Created on : Nov 17, 2011 by rtoso
+ * Authors    : Rodrigo Franco Toso <rtoso@cs.rutgers.edu>
+ *              Mauricio G.C. Resende <mgcr@research.att.com>
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018
+ * Rodrigo Franco Toso (rfrancotoso@gmail.com) and
+ * Mauricio G.C. Resende
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include "SampleDecoder.h"
+
+SampleDecoder::SampleDecoder() { }
+
+SampleDecoder::~SampleDecoder() { }
+
+// Runs in O(n \log n):
+double SampleDecoder::decode(const std::vector< double >& chromosome) const {
+	double myFitness = 0.0;
+	typedef std::pair< double, unsigned > ValueKeyPair;
+	std::vector< ValueKeyPair > rank(chromosome.size());
+
+	for(unsigned i = 0; i < chromosome.size(); ++i) {
+		rank[i] = ValueKeyPair(chromosome[i], i);
+		myFitness += (double(i + 1) * chromosome[i]);
+	}
+
+	// Here we sort 'permutation', which will then produce a permutation of [n]
+	// stored in ValueKeyPair::second:
+	std::sort(rank.begin(), rank.end());
+
+	// permutation[i].second is in {0, ..., n - 1}; a permutation can be obtained as follows
+	std::list< unsigned > permutation;
+	for(std::vector< ValueKeyPair >::const_iterator i = rank.begin(); i != rank.end(); ++i) {
+		permutation.push_back(i->second);
+	}
+
+	// Return the fitness:
+	return myFitness;
+}
---
+++ src/api-usage.cpp
@@ -0,0 +1,103 @@
+/**
+ * api-usage.cpp
+ *
+ * Driver class with a simple example of how to instantiate and use the BRKGA API.
+ * See SampleDecoder.h for details on the decoder's implementation.
+ *
+ * Created on : Nov 17, 2011 by rtoso
+ * Authors    : Rodrigo Franco Toso <rtoso@cs.rutgers.edu>
+ *              Mauricio G.C. Resende <mgcr@research.att.com>
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018
+ * Rodrigo Franco Toso (rfrancotoso@gmail.com) and
+ * Mauricio G.C. Resende
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include "RcppArmadillo.h"
+
+// via the depends attribute we tell Rcpp to create hooks for
+// RcppArmadillo so that the build process will know what to do
+//
+// [[Rcpp::depends(RcppArmadillo)]]
+
+//#include <iostream>
+#include <algorithm>
+#include "BRKGA.h"
+#include "MTRand.h"
+#include "SampleDecoder.h"
+
+// via the exports attribute we tell Rcpp to make this function
+// available from R
+//
+// [[Rcpp::export]]
+int api_usage() {
+	Rcpp::Rcout << "Welcome to the BRKGA API sample driver.\nFinding a (heuristic) minimizer for "
+			<< " f(x) = sum_i (x_i * i) where x \\in [0,1)^n." << std::endl;
+			
+	const unsigned n = 10;		// size of chromosomes
+	const unsigned p = 100;		// size of population
+	const double pe = 0.10;		// fraction of population to be the elite-set
+	const double pm = 0.10;		// fraction of population to be replaced by mutants
+	const double rhoe = 0.70;	// probability that offspring inherit an allele from elite parent
+	const unsigned K = 3;		// number of independent populations
+	const unsigned MAXT = 1;	// number of threads for parallel decoding
+	
+	SampleDecoder decoder;				// initialize the decoder
+	
+	const long unsigned rngSeed = 0;	// seed to the random number generator
+	MTRand rng(rngSeed);				// initialize the random number generator
+	
+	// initialize the BRKGA-based heuristic
+	BRKGA< SampleDecoder, MTRand > algorithm(n, p, pe, pm, rhoe, decoder, rng, K, MAXT);
+	
+	unsigned generation = 0;		// current generation
+	const unsigned X_INTVL = 100;	// exchange best individuals at every 100 generations
+	const unsigned X_NUMBER = 2;	// exchange top 2 best
+	const unsigned MAX_GENS = 1000;	// run for 1000 gens
+	std::cout << "Running for " << MAX_GENS << " generations..." << std::endl;
+	do {
+		algorithm.evolve();	// evolve the population for one generation
+		
+		if((++generation) % X_INTVL == 0) {
+			algorithm.exchangeElite(X_NUMBER);	// exchange top individuals
+		}
+	} while (generation < MAX_GENS);
+	
+	// print the fitness of the top 10 individuals of each population:
+	Rcpp::Rcout << "Fitness of the top 10 individuals of each population:" << std::endl;
+	const unsigned bound = std::min(p, unsigned(10));	// makes sure we have 10 individuals
+	for(unsigned i = 0; i < K; ++i) {
+	  Rcpp::Rcout << "Population #" << i << ":" << std::endl;
+		for(unsigned j = 0; j < bound; ++j) {
+		  Rcpp::Rcout << "\t" << j << ") "
+					<< algorithm.getPopulation(i).getFitness(j) << std::endl;
+		}
+	}
+	
+	Rcpp::Rcout << "Best solution found has objective value = "
+	 		<< algorithm.getBestFitness() << std::endl;
+	
+	
+	return 0;
+}
