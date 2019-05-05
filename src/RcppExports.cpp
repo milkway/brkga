1f555b5207bfc227f31a077e21989a7f8c512b1c
milkway <leite@de.ufpe.br>
2019-05-04 22:16:16 -0300

    Improving local search

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,40 +6,31 @@
 
 using namespace Rcpp;
 
-// tsp_brkga
-Rcpp::List tsp_brkga(std::string instanceFile);
-RcppExport SEXP _brkga_tsp_brkga(SEXP instanceFileSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< std::string >::type instanceFile(instanceFileSEXP);
-    rcpp_result_gen = Rcpp::wrap(tsp_brkga(instanceFile));
-    return rcpp_result_gen;
-END_RCPP
-}
 // mdp_brkga
-Rcpp::List mdp_brkga(const arma::mat DistanceMatrix, const unsigned m, const unsigned method, const unsigned MAX_TIME, const unsigned p, const double pe, const double pm, const double rhoe, const unsigned K, const unsigned MAXT, const unsigned X_INTVL, const unsigned X_NUMBER, const unsigned MAX_GENS, const unsigned RESET_AFTER, const bool verbose, const long unsigned rngSeed);
-RcppExport SEXP _brkga_mdp_brkga(SEXP DistanceMatrixSEXP, SEXP mSEXP, SEXP methodSEXP, SEXP MAX_TIMESEXP, SEXP pSEXP, SEXP peSEXP, SEXP pmSEXP, SEXP rhoeSEXP, SEXP KSEXP, SEXP MAXTSEXP, SEXP X_INTVLSEXP, SEXP X_NUMBERSEXP, SEXP MAX_GENSSEXP, SEXP RESET_AFTERSEXP, SEXP verboseSEXP, SEXP rngSeedSEXP) {
+Rcpp::List mdp_brkga(const arma::mat DistanceMatrix, const unsigned m, const unsigned method, const unsigned LS_INTERVAL, const unsigned MAX_TIME, const unsigned p, const double pe, const double pm, const double rhoe, const double lambda, const unsigned K, const unsigned THREADS, const unsigned X_INTVL, const unsigned X_NUMBER, const unsigned MAX_GENS, const unsigned RESET_AFTER, const bool verbose, const long unsigned rngSeed);
+RcppExport SEXP _brkga_mdp_brkga(SEXP DistanceMatrixSEXP, SEXP mSEXP, SEXP methodSEXP, SEXP LS_INTERVALSEXP, SEXP MAX_TIMESEXP, SEXP pSEXP, SEXP peSEXP, SEXP pmSEXP, SEXP rhoeSEXP, SEXP lambdaSEXP, SEXP KSEXP, SEXP THREADSSEXP, SEXP X_INTVLSEXP, SEXP X_NUMBERSEXP, SEXP MAX_GENSSEXP, SEXP RESET_AFTERSEXP, SEXP verboseSEXP, SEXP rngSeedSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< const arma::mat >::type DistanceMatrix(DistanceMatrixSEXP);
     Rcpp::traits::input_parameter< const unsigned >::type m(mSEXP);
     Rcpp::traits::input_parameter< const unsigned >::type method(methodSEXP);
+    Rcpp::traits::input_parameter< const unsigned >::type LS_INTERVAL(LS_INTERVALSEXP);
     Rcpp::traits::input_parameter< const unsigned >::type MAX_TIME(MAX_TIMESEXP);
     Rcpp::traits::input_parameter< const unsigned >::type p(pSEXP);
     Rcpp::traits::input_parameter< const double >::type pe(peSEXP);
     Rcpp::traits::input_parameter< const double >::type pm(pmSEXP);
     Rcpp::traits::input_parameter< const double >::type rhoe(rhoeSEXP);
+    Rcpp::traits::input_parameter< const double >::type lambda(lambdaSEXP);
     Rcpp::traits::input_parameter< const unsigned >::type K(KSEXP);
-    Rcpp::traits::input_parameter< const unsigned >::type MAXT(MAXTSEXP);
+    Rcpp::traits::input_parameter< const unsigned >::type THREADS(THREADSSEXP);
     Rcpp::traits::input_parameter< const unsigned >::type X_INTVL(X_INTVLSEXP);
     Rcpp::traits::input_parameter< const unsigned >::type X_NUMBER(X_NUMBERSEXP);
     Rcpp::traits::input_parameter< const unsigned >::type MAX_GENS(MAX_GENSSEXP);
     Rcpp::traits::input_parameter< const unsigned >::type RESET_AFTER(RESET_AFTERSEXP);
     Rcpp::traits::input_parameter< const bool >::type verbose(verboseSEXP);
     Rcpp::traits::input_parameter< const long unsigned >::type rngSeed(rngSeedSEXP);
-    rcpp_result_gen = Rcpp::wrap(mdp_brkga(DistanceMatrix, m, method, MAX_TIME, p, pe, pm, rhoe, K, MAXT, X_INTVL, X_NUMBER, MAX_GENS, RESET_AFTER, verbose, rngSeed));
+    rcpp_result_gen = Rcpp::wrap(mdp_brkga(DistanceMatrix, m, method, LS_INTERVAL, MAX_TIME, p, pe, pm, rhoe, lambda, K, THREADS, X_INTVL, X_NUMBER, MAX_GENS, RESET_AFTER, verbose, rngSeed));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -619,10 +610,20 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// tsp_brkga
+Rcpp::List tsp_brkga(std::string instanceFile);
+RcppExport SEXP _brkga_tsp_brkga(SEXP instanceFileSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< std::string >::type instanceFile(instanceFileSEXP);
+    rcpp_result_gen = Rcpp::wrap(tsp_brkga(instanceFile));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_brkga_tsp_brkga", (DL_FUNC) &_brkga_tsp_brkga, 1},
-    {"_brkga_mdp_brkga", (DL_FUNC) &_brkga_mdp_brkga, 16},
+    {"_brkga_mdp_brkga", (DL_FUNC) &_brkga_mdp_brkga, 18},
     {"_brkga_nl_brkga", (DL_FUNC) &_brkga_nl_brkga, 14},
     {"_brkga_Ackleys", (DL_FUNC) &_brkga_Ackleys, 1},
     {"_brkga_AluffiPentini", (DL_FUNC) &_brkga_AluffiPentini, 1},
@@ -672,6 +673,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_brkga_Zeldasine", (DL_FUNC) &_brkga_Zeldasine, 3},
     {"_brkga_reg_beta", (DL_FUNC) &_brkga_reg_beta, 2},
     {"_brkga_reg_brkga", (DL_FUNC) &_brkga_reg_brkga, 15},
+    {"_brkga_tsp_brkga", (DL_FUNC) &_brkga_tsp_brkga, 1},
     {NULL, NULL, 0}
 };
 
--- src/mdp_brkga.cpp
+++ src/mdp_brkga.cpp
@@ -1,27 +1,56 @@
 #include "RcppArmadillo.h"
-
 // via the depends attribute we tell Rcpp to create hooks for
 // RcppArmadillo so that the build process will know what to do
 //
 // [[Rcpp::depends(RcppArmadillo)]]
-
+// [[Rcpp::depends(RcppProgress)]]
+#include <progress.hpp>
+#include <progress_bar.hpp>
 
 #include <vector>
 #include <algorithm>
 #include <fstream>
 #include <iterator>
 #include <time.h>
+#include <functional>
 
 #include "MdpDecoder.h"
 #include "MTRand.h"
 #include "BRKGA.h"
 
-//#ifndef _OPENMP
-//#define _OPENMP 1
+
+
 // Used to sort ranking by second argument
 bool compare(const std::pair<double, int>&i, const std::pair<double, int>&j)
 {
   return i.second < j.second;
+}
+
+//Get M set
+std::set< unsigned > get_M(const std::vector< double >& chromosome, unsigned m){
+  std::vector< std::pair< double, unsigned > > ranking(chromosome.size());
+  
+  // ranking is the chromosome and vector of indices [0, 1, ..., n-1]
+  for(unsigned i = 0; i < chromosome.size(); ++i) {
+    ranking[i] = std::pair< double, unsigned >(chromosome[i], i);
+  }
+  
+  // Here we sort 'permutation', which will then produce a permutation of [n] in pair::second:
+  std::sort(ranking.begin(), ranking.end());
+  std::set< unsigned> M;
+  for(auto it = ranking.begin(); it != ranking.begin() + m; ++it) 
+    M.insert((*it).second);
+  return(M);
+}
+
+//Get N-M set
+std::set< unsigned > get_N(unsigned n, std::set< unsigned > M){
+  std::set< unsigned > N;
+  for(unsigned i = 0; i < n; i++){
+    auto it = M.find(i);
+    if (it == M.end()) N.insert(i);
+  }
+  return(N);
 }
 
 
@@ -51,12 +80,6 @@ Rcpp::List localSearch(const std::vector< double >& chromosome, double myFitness
 
   double LocalSearchFitness = myFitness;
 
-  // std::random_device rd;
-  // std::mt19937 g(rd());
-  // 
-  // std::shuffle(M.begin(), M.end(),g);
-  // std::shuffle(N_M.begin(), N_M.end(),g);
-  
   //First Improvement
   double delta_Z = 0;
   for(auto it_m = M.begin(); it_m != M.end(); ++it_m) {
@@ -101,38 +124,40 @@ Rcpp::List localSearch(const std::vector< double >& chromosome, double myFitness
 //' Execute a MDP solution search using BRKGA
 //' @details Escrever um detalhamento bem legal e super bacana
 //'  aqui.
-//' @param distances Distance matrix of the instance 
-//' @param m Number of elements selected (m <= n)
-//' @param MAX_TIME  Max time of execution in seconds
-//' @param p  Size of population
-//' @param pe Fraction of population to be the elite-set
-//' @param pm Fraction of population to be replaced by mutants
-//' @param rhoe Frobability that offspring inherit an allele from elite parent
-//' @param K number of independent populations
-//' @param MAXT number of threads for parallel decoding
-//' @param X_INTVL Exchange best individuals at every 100 generations
-//' @param X_NUMBER Exchange top 2 best
-//' @param MAX_GENS Max number of generations
-//' @param SEM_MELHORA  Pergunte para Geiza
-//' @param verbose Level of output information
-//' @param rngSeed Seed to the random number generator
+//' @param \code{DistanceMatrix} distances matrix of the instance 
+//' @param \code{m} Number of elements selected (m <= n)
+//' @param \code{MAX_TIME}  Max time of execution in seconds
+//' @param \code{p}  Size of population
+//' @param \code{pe} Fraction of population to be the elite-set
+//' @param \code{pm} Fraction of population to be replaced by mutants
+//' @param \code{rhoe} Frobability that offspring inherit an allele from elite parent
+//' @param \code{lambda} probability update population with local search
+//' @param \code{K} number of independent populations
+//' @param \code{THREADS} number of threads for parallel decoding
+//' @param \code{X_INTVL} Exchange best individuals at every 100 generations
+//' @param \code{X_NUMBER} Exchange top 2 best
+//' @param \code{MAX_GENS} Max number of generations
+//' @param \code{verbose} Level of output information
+//' @param \code{rngSeed} Seed to the random number generator
 //' @return A numeric vector of random values
 //' @seealso \code{\link{api-usage}} and \url{https://github.com/milkway/brkga}
 //' @export 
 // [[Rcpp::export]]
-Rcpp::List mdp_brkga(const arma::mat DistanceMatrix, 
-                     const unsigned m, // Number of elements selected (m <= n)
-                     const unsigned method =  0, // 0: BRKGA; 1: BRKGA + LS; 2: BRKGA(LS) 
+Rcpp::List mdp_brkga(const arma::mat   DistanceMatrix,  
+                     const unsigned                 m,   // Number of elements selected (m <= n)
+                     const unsigned       method =  0,   // 0: BRKGA; 1: BRKGA + LS; 2: BRKGA(LS) 
+                     const unsigned  LS_INTERVAL = 10,  // Generations between local searches 
                      const unsigned     MAX_TIME = 10,	 // run for 10 seconds
                      const unsigned            p = 500,	 // size of population
                      const double             pe = 0.20, // fraction of population to be the elite-set
                      const double             pm = 0.10, // fraction of population to be replaced by mutants
                      const double           rhoe = 0.70, // probability that offspring inherit an allele from elite parent
-                     const unsigned            K = 1,		 // number of independent populations
-                     const unsigned         MAXT = 8,    // number of threads for parallel decoding
+                     const double         lambda = 0.01, // probability update population with local search
+                     const unsigned            K = 3,		 // number of independent populations
+                     const unsigned      THREADS = 8,    // number of threads for parallel decoding
                      const unsigned      X_INTVL = 100,	 // exchange best individuals at every 100 generations
                      const unsigned     X_NUMBER = 2,	   // exchange top 2 best
-                     const unsigned     MAX_GENS = 100, // Max generations without improvement 
+                     const unsigned     MAX_GENS = 100,  // Max generations without improvement 
                      const unsigned RESET_AFTER = 200, 
                      const bool          verbose = false, 
                      const long unsigned rngSeed = 0	   // seed to the random number generator
@@ -142,16 +167,27 @@ Rcpp::List mdp_brkga(const arma::mat DistanceMatrix,
   unsigned n = DistanceMatrix.n_cols;				// size of chromosomes
   
   MTRand rng(rngSeed);				// initialize the random number generator
-  
+ 
+  // Rand generator for Pop and Allele
+  std::mt19937::result_type seed_K = rngSeed + 1;
+  std::mt19937::result_type seed_n = rngSeed + 2;
+  std::mt19937::result_type seed_u = rngSeed + 3;
+  auto K_rand = std::bind(std::uniform_int_distribution<unsigned>(0,K-1),
+                          std::mt19937(seed_K));
+  auto n_rand = std::bind(std::uniform_int_distribution<unsigned>(0,n-1),
+                          std::mt19937(seed_n));
+  auto u_rand = std::bind(std::uniform_real_distribution<double>(0,1),
+                             std::mt19937(seed_u));
+
   MdpDecoder decoder(DistanceMatrix, m);			// initialize the decoder
-  
-  
+
   // initialize the BRKGA-based heuristic
-  BRKGA< MdpDecoder, MTRand > algorithm(n, p, pe, pm, rhoe, decoder, rng, K, MAXT);
+  BRKGA< MdpDecoder, MTRand > algorithm(n, p, pe, pm, rhoe, decoder, rng, K, THREADS);
   
   unsigned generation = 0;	// current generation
-  double lastfitness = 0;
-  double gensLoosing = 0;
+  //double lastBKfitness = 0;
+  double lastLSfitness = 0;
+  int gensLoosing = 0;
   
   //m = n*0.10;			// número de elementos do subconjunto de máxima diversidade
   clock_t start_t, end_t;
@@ -161,67 +197,110 @@ Rcpp::List mdp_brkga(const arma::mat DistanceMatrix,
   double BestLocalSearchFitness = 0;
   unsigned relevantGeneration = 0;	// last relevant generation: best updated or reset called
   unsigned ImprovedSol = 0;
-  std::vector< unsigned > BestSolution;
+  std::set< unsigned > BestSolution;
   std::vector< double > BestChromosome(n);
   Rcpp::List search;
-  
+  Rprintf("+--------------+--------------+------------+---------+----------+\n");
+  Rprintf("| Best Fitness | Local Search | Generation | Loosing | Duration |\n");
   double loopTime = 0;
   do {
     gensLoosing++;
     algorithm.evolve();	// evolve the population for one generation
-    Rcpp::Rcout << "Best solution generation " << generation << " found has objective value = " << algorithm.getBestFitness() << std::endl;
+
     
-    // Local Search
-    if (algorithm.getBestFitness() != lastfitness){
-      search = localSearch(algorithm.getBestChromosome(), (double)algorithm.getBestFitness(), DistanceMatrix, m);
-      std::vector<double> cr = Rcpp::as<std::vector<double>>(search["Chromosome"]);
-      if (method == 2) 
-        algorithm.exchangeAlleles(search["Chromosome"], 0, n-1, search["lsFitness"]);
-      //std::list< unsigned > solution = search["Solution"];
-      if ((double)search["lsFitness"] < BestLocalSearchFitness) {
-        BestLocalSearchFitness = search["lsFitness"];
-        BestSolution = Rcpp::as<std::vector<unsigned>>(search["Solution"]);
-        BestChromosome = Rcpp::as<std::vector<double>>(search["Chromosome"]);
-        ImprovedSol++;
-        gensLoosing = 0;
-        relevantGeneration = generation;
-        //Rcpp::Rcout << "\n Inserting best of local search in population\n\n";
+    // New Local Search
+    if (generation % LS_INTERVAL == 0){
+      std::vector<double> chromosome = algorithm.getBestChromosome();
+      double LocalSearchFitness = (double)algorithm.getBestFitness();
+      std::vector< std::pair< double, unsigned > > ranking(n);
+      // ranking is the chromosome and vector of indices [0, 1, ..., n-1]
+      for(unsigned i = 0; i < n; ++i) {
+        ranking[i] = std::pair< double, unsigned >(chromosome[i], i);
       }
       
+      std::set< unsigned > M = get_M(algorithm.getBestChromosome(), m);
+      std::set< unsigned > N = get_N(n, M);
+      
+      //Look for improvements and update a population
+      //instance <- read_rds("inst/extdata/MDG.1.a.n500m50.rds")
+      double delta_Z = 0;
+      for(auto it_m = M.begin(); it_m != M.end(); ++it_m) {
+        for(auto it_n_m = N.begin(); it_n_m != N.end(); ++it_n_m) {
+          // calculando o delta z (vizinho - melhor_Solucao)
+          delta_Z = 0;
+          Rcpp::checkUserInterrupt();
+          for(auto item = M.begin(); item != M.end(); item++) {
+            if (*item != *it_m) 
+              delta_Z += -DistanceMatrix(*it_m, *item) + DistanceMatrix(*it_n_m, *item);
+          }
+          if (delta_Z > 0) {
+            M.insert(*it_n_m);
+            double aux = chromosome[*it_m];
+            chromosome[*it_m] = chromosome[*it_n_m];
+            chromosome[*it_n_m] = aux;
+            N.insert(*it_m);
+            M.erase(*it_m);
+            N.erase(*it_n_m);
+            //Update Fitness
+            LocalSearchFitness -= delta_Z;
+            it_m = M.begin();
+            it_n_m = N.begin();
+            // Update best chromossome
+            if (LocalSearchFitness < BestLocalSearchFitness){
+              BestChromosome = chromosome;
+              BestLocalSearchFitness = LocalSearchFitness;
+              BestSolution = M;
+              gensLoosing = 0;
+            }
+            //result <- mdp_brkga(DistanceMatrix = instance, m = 50, K = 3, MAX_TIME = 10, rngSeed = as.integer(Sys.time()))
+            if (u_rand() < lambda) 
+              algorithm.exchangeAlleles(chromosome, K_rand(), n_rand(), LocalSearchFitness);
+            Rprintf("\r| %12.2f | %12.2f | %10i | %7i | %8.0f |", \
+                    algorithm.getBestFitness(),                   \
+                    BestLocalSearchFitness,                       \
+                    generation,                                   \
+                    gensLoosing,                                  \
+                    (double)(difftime(clock(),start_t)/CLOCKS_PER_SEC));
+            Rprintf("\n+--------------+--------------+------------+---------+----------+\r\b\r");
+          } 
+        }
+      }
     }
-    
-    Rcpp::Rcout << "Local Search  of generation : " << BestLocalSearchFitness << "*********" << std::endl;
-    Rcpp::Rcout << "--------------" << std::endl;
-    
-    lastfitness = algorithm.getBestFitness();
+    if (lastLSfitness < BestLocalSearchFitness){      
+      ImprovedSol++;
+    } 
+
+    //lastBKfitness = algorithm.getBestFitness();
+    lastLSfitness = BestLocalSearchFitness;
     
     //  Evolution strategy: restart
     if(generation - relevantGeneration > RESET_AFTER) {
       algorithm.reset();	// restart the algorithm with random keys
       relevantGeneration = generation;
-      
-      Rcpp::Rcout << "\t" << generation << ") Reset at generation "
-                  << generation << std::endl;
-    }
+      }
     
     if((++generation) % X_INTVL == 0) {
       algorithm.exchangeElite(X_NUMBER);	// exchange top individuals
     }
     end_t = clock();
     loopTime = (double)(difftime(end_t,start_t)/CLOCKS_PER_SEC);
-    //} while (generation < MAX_GENS);
+    Rprintf("\r| %12.2f | %12.2f | %10i | %7i | %8.0f |", \
+            algorithm.getBestFitness(),                   \
+            BestLocalSearchFitness,                       \
+            generation,                                   \
+            gensLoosing,                                  \
+            (double)(difftime(clock(),start_t)/CLOCKS_PER_SEC));
+    Rprintf("\n+--------------+--------------+------------+---------+----------+\r\b\r");
   } while ((loopTime <= MAX_TIME) && (gensLoosing <= MAX_GENS));
-  
-  Rcpp::Rcout << "Best solution found has objective value = " << algorithm.getBestFitness() << "em " << difftime(end_t,start_t)/CLOCKS_PER_SEC << " segundos" << std::endl << std::endl;
+  Rprintf("\n\nThis is the end. The Doors.\n");
   Rcpp::List rst = Rcpp::List::create(
     Rcpp::Named("LSChr") = BestChromosome,
     Rcpp::Named("BKChr") = algorithm.getBestChromosome(),
     Rcpp::Named("Tour") = BestSolution,
-    Rcpp::Named("Fitness") = algorithm.getBestFitness(),
-    Rcpp::Named("lsFitness") = BestLocalSearchFitness,
+    Rcpp::Named("LSFitness") = algorithm.getBestFitness(),
+    Rcpp::Named("BKFitness") = BestLocalSearchFitness,
     Rcpp::Named("Generations Number") = generation,
     Rcpp::Named("Improvement Number") = ImprovedSol,
     Rcpp::Named("Duration") = loopTime);
-  
   return rst;
   }
